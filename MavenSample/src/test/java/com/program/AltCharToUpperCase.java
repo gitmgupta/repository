6895e2a854644be9226e8e5d04f730ff6d08c3a4
Admin <Admin@Admin-PC>
2019-08-18 15:29:01 +0530

    altchar to upper case

--- MavenSample/src/test/java/com/program/AltCharToUpperCase.java
+++ MavenSample/src/test/java/com/program/AltCharToUpperCase.java
@@ -1,5 +1,21 @@
 package com.program;
 
+import java.util.Scanner;
+
 public class AltCharToUpperCase {
 
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the value: ");
+		String value = scan.next();
+		String word = "";
+		for (int i = 0; i < value.length(); i++) {
+			char ch = value.charAt(i);
+			if (i % 2 == 0) {
+				ch = Character.toUpperCase(ch);
+				word = word + ch;
+			}
+			System.out.println(word);
+		}
+	}
 }
